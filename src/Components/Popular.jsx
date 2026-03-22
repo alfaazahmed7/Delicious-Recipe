682a059f05165b1820b7eb5daa5db951829d362f
Alfaaz Ahmed <alfaazahmed010@gmail.com>
2026-03-22 11:41:15 +0600

    display image and title from API

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-const Popular = () => {
+function Popular() {
+    const [popular, setPopular] = useState([]);
+
+    useEffect(() => {
+        getPopular();
+    }, []);
+
+    const getPopular = async () => {
+        const api = await fetch(
+            `https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_API_KEY}&number=9`);
+        const data = await api.json();
+        setPopular(data.recipes);
+    }
+
     return (
         <div>
-            <h4>Popular</h4>
+            {popular.map((recipe) => {
+                return (
+                    <Wrapper>
+                        <h3>Popular Picks</h3>
+                        {popular.map((recipe) => {
+                            return (
+                                <Card>
+                                    <p>{recipe.title}</p>
+                                    <img src={recipe.image} alt={recipe.title} />
+                                </Card>
+                            )
+                        })}
+                    </Wrapper>
+                )
+            })}
         </div>
     );
-};
+}
+
+const Wrapper = styled.div`
+    margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+    min-height: 25rem;
+    border-radius: 2rem;
+    overflow: hidden;
+    img {
+        border-radius: 2rem;
+    }
+`;
 
 export default Popular;
